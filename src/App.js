c09b39e544de46bf0f75ea7d53ff851c86878242
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2023-07-06 01:17:52 +0100

    [CHORE]-additions

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import WidgetGalleryModal from './modals/WidgetGalleryModal';
 import Draggable from 'react-draggable';
 import TimerWidget from './widgets/TimerWidget';
 import CalendarWidget from './widgets/CalendarWidget';
- 
+ import MyCustomWidget from './widgets/MyCustomWidget';
 function App() {
  const [widgets, setWidgets] = useState([
   { id: new Date().getTime(), component: <ClockWidget />, area: 'main-widget', name: "Date and Time" },
   { id: new Date().getTime() + 2, component: <CalendarWidget />, area: 'right-widget', name: "Calendar" },
   { id: new Date().getTime() + 1, component: <TimerWidget />, area: 'left-widget', name:"Timer" },
+  { id: new Date().getTime() + 3, component: <MyCustomWidget />, area: 'Floating-widget', name: "Note" },
  ])
  const [showWidgetModal, setShowWidgetModal] = useState(false)
  const [selectedWidgetArea, setSelectedWidgetArea] = useState('')
@@ -154,6 +155,45 @@ function App() {
      <div className='right-widget'>
       {widgets.length > 0 && widgets.map((widget, index) => {
        if (widget.area === 'right-widget') {
+        return (
+         <div style={{ padding: 10,}} className='widget-container'>
+          <div style={{ marginBottom: 10 }}>
+           <select value={widget.area}
+            onChange={(e) => {
+             let widgetList = [...widgets]
+             widgetList[index].area = e.target.value
+             setWidgets(widgetList)
+            }}
+           >
+            <option value="none-widget">Floating</option>
+            <option value="left-widget">Left</option>
+            <option value="main-widget">Main</option>
+            <option value="right-widget">Right</option>
+           </select>
+           <button
+            onClick={() => {
+             removeWidget(widget.id)
+            }}
+            style={{ background: "none", border: "none", color: "white", fontSize: "15px", cursor: "pointer", float: "right" }}
+           >Close</button>
+          </div>
+          {widget.component}
+         </div>
+        )
+       }
+      })}
+      {widgets.filter(widget => widget.area === 'right-widget').length < 3 && widgets.filter(widget => widget.area === 'right-widget').length > 0 &&
+       <button
+        className='add-widget-button'
+        onClick={() => {
+         setShowWidgetModal(true)
+         setSelectedWidgetArea('right-widget')
+        }}>+ Add Widget</button>
+      }
+     </div>
+     <div className='none-widget'>
+      {widgets.length > 0 && widgets.map((widget, index) => {
+       if (widget.area === 'none-widget') {
         return (
          <div style={{ padding: 10,}} className='widget-container'>
           <div style={{ marginBottom: 10 }}>
